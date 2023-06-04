4ec612b440a5c9bc5b0f5eeb44237af215f76d0a
CheChristobal <che.christobal@gmail.com>
2023-06-04 19:17:45 -0400

    Commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import paisaje from './Placeholder.png';
+import animales from './Placeholder.png';
+import comida from './Placeholder.png';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      title: 'Hermoso Paisaje',
+      image: paisaje,
+      description: 'Este es un paisaje impresionante.',
+      date: '2023-05-30'
+    },
+    {
+      id: 2,
+      title: 'Animales Adorables',
+      image: animales,
+      description: 'Estos animales son adorables.',
+      date: '2023-05-31'
+    },
+    {
+      id: 3,
+      title: 'Comida Deliciosa',
+      image: comida,
+      description: 'Esta comida se ve deliciosa.',
+      date: '2023-06-01'
+    },
+    {
+      id: 4,
+      title: 'Título de la imagen 1',
+      image: comida,
+      description: 'Descripción de la imagen 1.',
+      date: '2023-06-02'
+    },
+    {
+      id: 5,
+      title: 'Título de la imagen 2',
+      image: comida,
+      description: 'Descripción de la imagen 2.',
+      date: '2023-06-03'
+    },
+    {
+      id: 6,
+      title: 'Título de la imagen 3',
+      image: comida,
+      description: 'Descripción de la imagen 3.',
+      date: '2023-06-04'
+    },
+    {
+      id: 7,
+      title: 'Título de la imagen 4',
+      image: comida,
+      description: 'Descripción de la imagen 4.',
+      date: '2023-06-05'
+    },
+    {
+      id: 8,
+      title: 'Título de la imagen 5',
+      image: comida,
+      description: 'Descripción de la imagen 5.',
+      date: '2023-06-06'
+    },
+    {
+      id: 9,
+      title: 'Título de la imagen 6',
+      image: comida,
+      description: 'Descripción de la imagen 6.',
+      date: '2023-06-07'
+    },
+    {
+      id: 10,
+      title: 'Título de la imagen 7',
+      image: comida,
+      description: 'Descripción de la imagen 7.',
+      date: '2023-06-08'
+    },
+    {
+      id: 11,
+      title: 'Título de la imagen 8',
+      image: comida,
+      description: 'Descripción de la imagen 8.',
+      date: '2023-06-09'
+    },
+    {
+      id: 12,
+      title: 'Título de la imagen 9',
+      image: comida,
+      description: 'Descripción de la imagen 9.',
+      date: '2023-06-10'
+    },
+    {
+      id: 13,
+      title: 'Título de la imagen 10',
+      image: comida,
+      description: 'Descripción de la imagen 10.',
+      date: '2023-06-11'
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <h1>Aplicación De Imagenes</h1>
       </header>
+      <div className="card-container">
+        {cards.map((card) => (
+          <div key={card.id} className="card">
+            <h2 className="card-title">{card.title}</h2>
+            <img className="card-image" src={card.image} alt={card.title} />
+            <p className="card-description">{card.description}</p>
+            <p className="card-date">Publicado en: {card.date}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
